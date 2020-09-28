120568af7550c200e332c5ce0962c5a52006181e
prashmittanay <prashmittanay@gmail.com>
2020-09-28 19:21:52 +0530

    logic to increment questions added

--- app/src/main/java/org/learn/quizapp/MainActivity.java
+++ app/src/main/java/org/learn/quizapp/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import beans.Question;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private TextView mQuestionTextView;
+    private Button mNextButton;
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -27,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate() called!");
         setContentView(R.layout.activity_main);
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mNextButton = findViewById(R.id.next_button);
 
+        addListeners();
+        updateQuestion();
+    }
+
+    private void addListeners() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 }
