e5bf4c568593ffbe5eea65e2611edc27b84792b4
prashmittanay <prashmittanay@gmail.com>
2020-09-28 18:36:19 +0530

    Questions mapped to main activity

--- app/src/main/java/org/learn/quizapp/MainActivity.java
+++ app/src/main/java/org/learn/quizapp/MainActivity.java
@@ -5,9 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import beans.Question;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
