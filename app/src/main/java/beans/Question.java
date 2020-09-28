79e115ccdc58d97637148549c313a48a4125bf76
prashmittanay <prashmittanay@gmail.com>
2020-09-28 18:32:21 +0530

    Question bean added

---
+++ app/src/main/java/beans/Question.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        this.mTextResId = textResId;
+        this.mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
--- app/src/main/java/org/learn/quizapp/MainActivity.java
+++ app/src/main/java/org/learn/quizapp/MainActivity.java
@@ -3,12 +3,18 @@ package org.learn.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called!");
         setContentView(R.layout.activity_main);
+
+
     }
 }
